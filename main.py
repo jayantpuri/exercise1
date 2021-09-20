e1e7ebdba010835f97c882981236a47c3c91a0ef
jayantpuri <jayantpuri16@gmail.com>
2021-09-21 01:04:27 +0530

    added upper case feature

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ Created on Tue Sep 21 00:41:24 2021
 @author: slayer
 """
 from lowerCase import lowerCase
+from upperCase import upperCase
 str= input("Enter the string you want to print")
 
 choice = int(input("press 1 for lower cae and press 2 for upper case"))
 if (choice==1):
     print(lowerCase(str))
-    
 
-
-
+elif(choice==2):
+    print(upperCase(str))
---
+++ upperCase.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 01:01:17 2021
+
+@author: slayer
+"""
+
+def upperCase(str):
+    return str.upper()
