ccb56f8843e8316c3f5de2b84b8d51033aa713fe
jayantpuri <jayantpuri16@gmail.com>
2021-09-21 00:51:02 +0530

    added the main structure

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 00:41:24 2021
+
+@author: slayer
+"""
+
+str= input("Enter the string you want to print")
+
+choice = int(input("press 1 for lower cae and press 2 for upper case"))
+
+
+
